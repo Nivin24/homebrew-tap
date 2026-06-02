2140621bfc849638ca60b12123132da7ecc8a2cc
Nivin Benny <134007084+Nivin24@users.noreply.github.com>
2026-06-02 16:11:37 +0530

    Update download URL for Cyro cask

--- Casks/cyro.rb
+++ Casks/cyro.rb
@@ -2,7 +2,7 @@ cask "cyro" do
   version "1.0.0"
   sha256 "33568950f08e0fd763d2797ffe4df9241a758e0b465e7a570e7f58d830d8034c"
 
-  url "https://github.com/Nivin24/cyro-macos-agent/releases/download/v#{version}/Cyro.dmg"
+  url "https://github.com/Nivin24/cyro-releases/releases/download/v#{version}/Cyro.dmg"
   name "Cyro"
   desc "Ambient Multi-Modal macOS Agent"
   homepage "https://github.com/Nivin24/cyro-macos-agent"
